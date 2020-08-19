0920d0024da23126f05089cb553be98d7266f4c2
Pablo <desenvolvimento7@kurole.com>
2020-08-19 17:48:14 -0300

    initial repository

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+
+import MobileHeader from '../MobileHeader';
+import LeftColumn from '../LeftColumn';
+import MiddleColumn from '../MiddleColumn';
+import RightColumn from '../RightColumn';
+
+const Layout: React.FC = () => {
+  return (
+          <Container>
+            <main>
+              <LeftColumn />
+              <MiddleColumn />
+              <RightColumn />
+            </main>
+          </Container>
+        )
+  ;
+}
+
+export default Layout;
